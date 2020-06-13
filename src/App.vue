14c4d1adac33b8ef78902aef0f77c873635b1838
FinnTenzor <finntenzor@gmail.com>
2020-06-14 00:17:48 +0800

    抄一个navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <div class="navbar">
+      <div class="navbar-inner">
+        <a class="navbar-title" href="https://www.sepeach.com/">桃按钮</a>
+        <a class="navbar-subtitle" href="https://rec.sepeach.com/">24H录播站</a>
+      </div>
+    </div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -9,6 +15,14 @@
 </template>
 
 <style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+a {
+  text-decoration: none;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -28,5 +42,29 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.navbar {
+  height: 40px;
+  padding: 8px 0;
+  background-color: #4582ec;
+}
+.navbar-inner {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  width: 1110px;
+  margin: 0 auto;
+  text-align: left;
+}
+.navbar-title {
+  margin-right: 16px;
+  color: #fff;
+  font-size: 22px;
+}
+.navbar-subtitle {
+  padding: 8px;
+  color: rgb(255,255,255,0.6);
+  font-size: 14px;
 }
 </style>
