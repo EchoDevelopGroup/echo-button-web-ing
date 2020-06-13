c9d8bd14e9521cc4350749ef62d1b1184404af54
FinnTenzor <finntenzor@gmail.com>
2020-06-13 22:35:33 +0800

    添加axios依赖

---
+++ src/api/request.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+// 创建axios实例
+const _axios = axios.create({
+  baseURL: '/echo/project/echo', // api的base_url
+  timeout: 5000 // 请求超时时间
+})
+
+// response拦截器==>处理后端返回相应
+_axios.interceptors.response.use(
+  response => {
+    if (response) {
+      if (response.data) {
+        const data = response.data
+        if (data.code === 0) {
+          return data.data
+        } else {
+          return Promise.reject(new Error(data.message))
+        }
+      } else {
+        return Promise.reject(new Error('网络异常'))
+      }
+    } else {
+      return Promise.reject(new Error('运行时异常'))
+    }
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+export default _axios
