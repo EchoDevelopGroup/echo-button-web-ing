946737d187cea150381c47f924843c145df36de0
FinnTenzor <finntenzor@gmail.com>
2020-06-13 22:51:54 +0800

    加入element ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+import 'element-ui/lib/theme-chalk/index.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
