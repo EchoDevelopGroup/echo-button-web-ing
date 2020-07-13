a8ec65a1e687f76f995418ddb1ac3e24499c2e57
FinnTenzor <finntenzor@gmail.com>
2020-07-13 15:40:45 +0800

    feature: 导航栏

---
+++ src/components/AppNavigator.vue
@@ -0,0 +1,85 @@
+<template>
+  <!-- 整个网站的导航栏 顶部写着网址的那个 -->
+  <div class="app-navigator primary-shadow">
+    <div class="app-navigator-frame">
+      <!-- 桃按钮网站图标 -->
+      <a href="" class="app-navigator-logo-link">
+        <img src="@/assets/logo-horizontal.png" alt="logo" title="桃按钮" class="app-navigator-logo">
+      </a>
+
+      <!-- 录播站按钮 -->
+      <a href="https://rec.sepeach.com/" class="app-navigator-link-button primary-shadow">Echo录播存放处</a>
+
+      <!-- 占满水平剩余宽度的占位div -->
+      <div class="app-navigator-placeholder"></div>
+
+      <!-- 音量和语言按钮 -->
+      <div class="app-navigator-button">
+        <img src="@/assets/icon/volume.png" alt="音量" class="app-navigator-button-img">
+      </div>
+      <div class="app-navigator-button">
+        <img src="@/assets/icon/language.png" alt="语言" class="app-navigator-button-img">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppNavigator'
+}
+</script>
+
+<style>
+.app-navigator {
+  width: 100%;
+  height: 50px;
+  background-color: #fe76a1;
+  background: linear-gradient(to right, #fec2d2, #fe6990);
+}
+/* 加一层框 宽度等于 1110 + 70 * 2 看起来好看一点 */
+.app-navigator-frame {
+  width: 1250px;
+  height: 100%;
+  margin: 0 auto;
+  padding: 0 25px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.app-navigator-logo-link {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.app-navigator-logo {
+  width: auto;
+  height: 40px;
+}
+.app-navigator-link-button {
+  margin-left: 20px;
+  padding: 6px 20px;
+  border-radius: 6px;
+  color: #fe7ba5;
+  background-color: #fff;
+}
+.app-navigator-placeholder {
+  flex: 1 0 auto;
+  height: 100%;
+}
+.app-navigator-button {
+  display: flex;
+  height: 50px;
+  width: 50px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+.app-navigator-button-img {
+  display: block;
+  width: auto;
+  height: 33px;
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,9 +2,7 @@
   <!-- 先用一个大概的划一下布局 -->
   <div class="main-layout">
     <!-- 导航栏 -->
-    <div class="navigator">
-
-    </div>
+    <app-navigator></app-navigator>
 
     <!-- 页面主要内容 白底的部分 -->
     <div class="page-container">
@@ -14,8 +12,13 @@
 </template>
 
 <script>
+import AppNavigator from '@/components/AppNavigator'
+
 export default {
-  name: 'MainLayout'
+  name: 'MainLayout',
+  components: {
+    AppNavigator
+  }
 }
 </script>
 
@@ -26,11 +29,6 @@ export default {
 </style>
 
 <style scoped>
-.navigator {
-  width: 100%;
-  height: 50px;
-  background-color: #fe709c;
-}
 .page-container {
   width: 1110px;
   min-height: 600px;
