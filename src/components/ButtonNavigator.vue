115f111d81f18df4b783f433ba24b6faabe835ac
FinnTenzor <finntenzor@gmail.com>
2020-07-13 16:36:03 +0800

    feature: 编写导航组

---
+++ src/components/ButtonNavigator.vue
@@ -0,0 +1,97 @@
+<template>
+  <!-- 按钮导航栏 用于切换页面 -->
+  <div class="button-navigator primary-shadow">
+    <ul class="button-navigator-list">
+      <!-- 最开头的黑桃 -->
+      <li class="button-navigator-item" @click="handleClick(0)">
+        <div class="button-navigator-main" @click.stop="handleClick(-1)">
+          <img src="@/assets/icon/spade-white.png" alt="spade" class="button-navigator-spade-img">
+        </div>
+      </li>
+
+      <!-- 各个按钮 -->
+      <li
+        v-for="(item, index) in buttonList"
+        :key="index"
+        @click="handleClick(index + 1)"
+        class="button-navigator-item"
+      >
+        <div class="button-navigator-main" @click.stop="handleClick(index)">
+          <span class="button-navigator-text">{{ item }}</span>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonNavigator',
+  props: {
+    buttonList: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    // 用户点击某一个按钮
+    handleClick(index) {
+      // 抛出事件 参数是第几个按钮 -1表示点了最前面的黑桃
+      this.$emit('click', index)
+    }
+  }
+}
+</script>
+
+<style>
+.button-navigator {
+  overflow: hidden;
+  height: 38px;
+  /* 最外层剪裁 用于去掉第一个按钮的左侧和最后一个按钮的右侧 */
+  border-top-left-radius: 23px; /* 22.8975 */
+  border-bottom-left-radius: 8px; /* 7.4125 */
+  border-bottom-right-radius: 28px; /* 27.735 */
+  border-top-right-radius: 4px; /* 4.1925 */
+}
+.button-navigator-list {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+}
+.button-navigator-item {
+  height: 100%;
+  cursor: pointer;
+}
+.button-navigator-main {
+  display: flex;
+  height: 100%;
+  padding: 0 28px 0 23px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border-bottom-right-radius: 28px; /* 27.735 */
+  border-top-right-radius: 4px; /* 4.1925 */
+}
+.button-navigator-item:nth-child(2n+1) {
+  background-color: #fd6e9a;
+}
+.button-navigator-item:nth-child(2n+1) > .button-navigator-main {
+  background-color: #e64e8c;
+}
+.button-navigator-item:nth-child(2n) {
+  background-color: #e64e8c;
+}
+.button-navigator-item:nth-child(2n) > .button-navigator-main {
+  background-color: #fd6e9a;
+}
+.button-navigator-spade-img {
+  width: auto;
+  height: 23px;
+}
+.button-navigator-text {
+  color: #fff;
+}
+</style>
--- src/layouts/ButtonLayout.vue
+++ src/layouts/ButtonLayout.vue
@@ -2,12 +2,8 @@
   <!-- 桃按钮和上传页面采用的布局 -->
   <div class="button-layout">
     <div class="navigator">
-      <ul class="button-list">
-        <li>黑桃</li>
-        <li>镇站之宝</li>
-        <li>软桃</li>
-        <li>通用</li>
-      </ul>
+      <button-navigator :button-list="buttons"></button-navigator>
+      <div style="height: 20px; width: 30px"></div>
       <button>上传</button>
       <button>审核</button>
     </div>
@@ -20,8 +16,24 @@
 </template>
 
 <script>
+import ButtonNavigator from '@/components/ButtonNavigator'
+
 export default {
-  name: 'ButtonLayout'
+  name: 'ButtonLayout',
+  components: {
+    ButtonNavigator
+  },
+  data() {
+    return {
+      buttons: [
+        '镇站之宝',
+        '软桃',
+        '通用',
+        'HSO',
+        '怪叫'
+      ]
+    }
+  }
 }
 </script>
 
