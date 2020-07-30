cdf8405b99fb0fb4d99fac5d2a4af8dd8a1c3ce2
FinnTenzor <finntenzor@gmail.com>
2020-07-30 22:45:23 +0800

    fix: 修复部署脚本

--- deploy.cmd
+++ deploy.cmd
@@ -1,5 +1,4 @@
 cd dist
-zip -r ./* dist.zip
-ssh root@173.82.243.121 rm -rf /www/wwwroot/www.sepeach.com/dist/*
-scp dist.zip root@173.82.243.121:/www/wwwroot/www.sepeach.com/dist/
-ssh root@173.82.243.121 cd /www/wwwroot/www.sepeach.com/dist/ && unzip dist.zip
+zip dist.zip -r ./*
+scp dist.zip root@173.82.243.121:/www/wwwroot/www.sepeach.com/
+ssh root@173.82.243.121 bash /www/wwwroot/www.sepeach.com/deploy.sh
