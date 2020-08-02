aaf0fda35ed23ca28d534cbe7e4e5ebc92729b44
FinnTenzor <finntenzor@gmail.com>
2020-08-02 18:42:08 +0800

    cleanup: 移动makeLocalStorage位置

--- src/store/index.js
+++ src/store/index.js
@@ -3,76 +3,12 @@ import Vuex from 'vuex'
 import * as api from '@/api'
 import { sha1 } from '@/util/sha1'
 import { Player } from '@/util/player'
+import { makeLocalStorage } from '@/util/storage'
 
 Vue.use(Vuex)
 
-const canLocalStorage = typeof window.localStorage !== 'undefined'
 const targetOverviewVersion = 1
 const targetConfigVersion = 2
-
-/**
- * 指定键名 返回该键名对应的LocalStorage封装
- * 调用has返回键值是否存在
- * 调用get返回本地存储的JSON
- * 调用set设置本地存储的JSON
- * @param {string} key 键名
- */
-function makeLocalStorage(key) {
-  const fetch = () => localStorage.getItem(key)
-  const remove = () => localStorage.removeItem(key)
-  const write = value => localStorage.setItem(key, value)
-  return {
-    /**
-     * 检查是否有本地存储
-     */
-    has() {
-      if (!canLocalStorage) {
-        return false
-      }
-      const v = fetch()
-      return typeof v === 'string'
-    },
-    /**
-     * 获取本地存储
-     * @returns {object|null}
-     */
-    get() {
-      if (!canLocalStorage) {
-        return null
-      }
-      const v = fetch()
-      if (typeof v === 'string') {
-        try {
-          const d = JSON.parse(v)
-          return d
-        } catch (err) {
-          console.log('[Storage]' + key + ' is no json, removed')
-          try {
-            remove()
-          } catch (err) {
-            console.log('[Storage]' + key + ' remove failed, ignore')
-          }
-        }
-      }
-      return null
-    },
-    /**
-     * 设置本地存储
-     * @param {*} d 数据
-     */
-    set(d) {
-      if (!canLocalStorage) {
-        return
-      }
-      try {
-        const v = JSON.stringify(d)
-        write(v)
-      } catch (err) {
-        console.log('[Storage]' + key + ' write failed, ignore')
-      }
-    }
-  }
-}
 
 const localOverview = makeLocalStorage('echo_button_overview')
 const localConfig = makeLocalStorage('echo_button_config')
